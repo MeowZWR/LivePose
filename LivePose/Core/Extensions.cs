a66524e1c410cbdec7ad0a47aadcc7f818e35bde
Caraxi <git@caraxian.com>
2025-09-27 18:41:11 +0930

    Update Dalamud SDK

--- LivePose/Core/Extensions.cs
+++ LivePose/Core/Extensions.cs
@@ -8,7 +8,8 @@ public static class Extensions {
         return condition.Any(ConditionFlag.BetweenAreas, ConditionFlag.CreatingCharacter, ConditionFlag.LoggingOut,
             ConditionFlag.BetweenAreas51, ConditionFlag.Transformed, ConditionFlag.WatchingCutscene, ConditionFlag.WatchingCutscene78,
             ConditionFlag.OccupiedInCutSceneEvent, ConditionFlag.BeingMoved, ConditionFlag.EditingPortrait, ConditionFlag.CarryingItem,
-            ConditionFlag.CarryingObject, ConditionFlag.Disguised);
+            ConditionFlag.CarryingObject, ConditionFlag.Disguised, ConditionFlag.Crafting, ConditionFlag.ExecutingCraftingAction,
+            ConditionFlag.Gathering, ConditionFlag.ExecutingGatheringAction);
     }
         
 }
