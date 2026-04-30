785998b341f218014f7087a237c9891148451900
Caraxi <git@caraxian.com>
2026-04-30 21:44:51 +0930

    API15

--- LivePose/UI/ImCallback.cs
+++ LivePose/UI/ImCallback.cs
@@ -5,22 +5,22 @@ using Dalamud.Interface.Utility.Raii;
 namespace LivePose.UI;
 
 public static class ImCallback {
-    public static void TreeNode(ImU8String label, Action callback, Action? afterDraw = null) => ImRaii.TreeNode(label).Callback(callback, afterDraw);
-    public static void TreeNode(ImU8String label, ImGuiTreeNodeFlags flags, Action callback, Action? afterDraw = null) => ImRaii.TreeNode(label, flags).Callback(callback, afterDraw);
-    public static void TabBar(ImU8String label, Action callback, Action? afterDraw = null) => ImRaii.TabBar(label).Callback(callback, afterDraw);
-    public static void TabBar(ImU8String label, ImGuiTabBarFlags flags, Action callback, Action? afterDraw = null) => ImRaii.TabBar(label, flags).Callback(callback, afterDraw);
-    public static void TabItem(ImU8String label, Action callback, Action? afterDraw = null) => ImRaii.TabItem(label).Callback(callback, afterDraw);
-    public static void TabItem(ImU8String label, ImGuiTabItemFlags flags, Action callback, Action? afterDraw = null) => ImRaii.TabItem(label, flags).Callback(callback, afterDraw);
-    public static void TabItem(ImU8String label, ref bool open, Action callback, Action? afterDraw = null) => ImRaii.TabItem(label, ref open).Callback(callback, afterDraw);
-    public static void TabItem(ImU8String label, ref bool open, ImGuiTabItemFlags flags, Action callback, Action? afterDraw = null) => ImRaii.TabItem(label, ref open, flags).Callback(callback, afterDraw);
-
-    private static void Callback<T>(this T endObj, Action callback, Action? afterDraw) where T : ImRaii.IEndObject {
-        try {
-            afterDraw?.Invoke();
-            if(endObj.Success) callback();
-        } finally {
-            endObj.Dispose();
-        }
+    public static void TreeNode(ImU8String label, Action callback, Action? afterDraw = null) {
+        using var node = ImRaii.TreeNode(label);
+        afterDraw?.Invoke();
+        if(node.Success) callback();
+    }
+    
+    public static void TabBar(ImU8String label, Action callback, Action? afterDraw = null)  {
+        using var node = ImRaii.TabBar(label);
+        afterDraw?.Invoke();
+        if(node.Success) callback();
+    }
+    
+    public static void TabItem(ImU8String label, Action callback, Action? afterDraw = null)  {
+        using var node = ImRaii.TabItem(label);
+        afterDraw?.Invoke();
+        if(node.Success) callback();
     }
 
 }
